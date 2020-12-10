897e0a978ce152f03ac8a6a439c919708dec4fb4
Garrett Lee <rgl10d@gmail.com>
2020-12-10 18:26:10 -0500

    created the connection.js file

--- models/room.js
+++ models/room.js
@@ -1,7 +1,9 @@
+// CREATE ROOM MODEL
 module.exports = function (sequelize, DataTypes) {
     const Room = sequelize.define("Room", {
         name: DataTypes.STRING,
         status: DataTypes.BOOLEAN,
     });
+// EXPORT MODEL TO CONTROLLER 
     return Room;
 };
