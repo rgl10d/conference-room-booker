098e3b1dcb1e3f52b092e381c6d7065e42b21849
Irene Asay <irene.hsu.asay@gmail.com>
2020-12-11 00:05:54 +0800

    created rooms_db and rooms table in Workbench

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,13 @@
-DROP DATABASE IF EXISTS test_db;
-CREATE DATABASE test_db;
+DROP DATABASE IF EXISTS rooms_db;
+CREATE DATABASE rooms_db;
+USE rooms_db;
+
+CREATE TABLE `rooms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `dateCreated` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  )
+  
+  SELECT * FROM rooms_db.rooms;
