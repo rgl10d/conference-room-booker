877cf6cbd28069ac264fbc0ab6c94a6a8e4e8200
Irene Asay <irene.hsu.asay@gmail.com>
2020-12-11 18:04:24 +0800

    created reserve_db database locally

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,3 @@
-DROP DATABASE IF EXISTS rooms_db;
-CREATE DATABASE rooms_db;
-USE rooms_db;
+CREATE DATABASE reserve_db;
+USE reserve_db;
 
-CREATE TABLE `rooms` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `dateCreated` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-  )
-  
-  SELECT * FROM rooms_db.rooms;
