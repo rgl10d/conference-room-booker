055061bc2b9b113ccdfb5271ced8dac04ef92448
Irene Asay <irene.hsu.asay@gmail.com>
2020-12-11 23:08:08 +0800

    added seed.sql with data

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+SELECT * FROM reserve_db.Rooms;
+Use reserve_db;
+insert into Rooms (name, status, createdAt, updatedAt) values ("Gold Room", false, "2020-11-11 20:12:54", "2020-11-15 20:12:54");
+insert into Rooms (name, status, createdAt, updatedAt) values ("Siver Room", false, "2020-11-15 20:12:54", "2020-11-15 20:12:54");
+insert into Rooms (name, status, createdAt, updatedAt) values ("Platinum Room", false, "2020-11-18 20:12:54", "2020-11-21 20:12:54");
+
