701ea31c607e33c2f85c3fd638e137bab624602c
Irene Asay <irene.hsu.asay@gmail.com>
2020-12-11 22:18:20 +0800

    commented out the table in Schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,18 @@
 CREATE DATABASE reserve_db;
 USE reserve_db;
+
+-- rooms table
+-- id(PRIMARY)
+-- name(STRING)
+-- Status (BOOLEAN): available / not available
+-- timestamp (createdAt?)
+ 
+CREATE TABLE `rooms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_name` varchar(45) NOT NULL,
+  `status` BOOLEAN DEFAULT false,
+  `date-created` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  )
+  
+  SELECT * FROM rooms;
