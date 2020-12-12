5cbd679df83af47f12129bb2be1c5db9e459aa1c
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-12 12:38:43 -0500

    Added the new-form view route to the server.js file.

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+//to show the create new room form
+app.get("/new", (req, res) => {
+  res.render("new-room");
+});
+
+
 app.use(roomController);
 
 // API Routes
