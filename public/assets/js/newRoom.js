83cf72ef00a1105bb1cd2511c212165efd66a2df
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-13 15:43:21 -0500

    Added the document.ready function to the newRoom.js page.

--- public/assets/js/newRoom.js
+++ public/assets/js/newRoom.js
@@ -1,4 +1,6 @@
 // put event listener on the create room button
 //Create an object that sends the form info into the req.body
 // Use post request to take in the data from the new-room form
-console.log("I am working");
+$( document ).ready(function() {
+    console.log( "I am still working.");
+});
