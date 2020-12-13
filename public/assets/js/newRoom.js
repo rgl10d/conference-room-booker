49dfb5e05e66bacb7627bd2e32137d7f228903f3
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-13 15:41:13 -0500

    Put my password into the config.json, linked the new-room handlebars to the newRoom.js file and confirmed the files were properly linked.

--- public/assets/js/newRoom.js
+++ public/assets/js/newRoom.js
@@ -1,3 +1,4 @@
 // put event listener on the create room button
 //Create an object that sends the form info into the req.body
 // Use post request to take in the data from the new-room form
+console.log("I am working");
