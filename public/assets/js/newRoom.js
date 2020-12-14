96ee3142b502a1631f98f753548505335eff54af
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-14 14:41:51 -0500

    Added a new js file for changing the check-in button.

--- public/assets/js/newRoom.js
+++ public/assets/js/newRoom.js
@@ -1,6 +1,3 @@
-// put event listener on the create room button
-//Create an object that sends the form info into the req.body
-// Use post request to take in the data from the new-room form
 $( document ).ready(function() {
     console.log( "I am still working.");
 
