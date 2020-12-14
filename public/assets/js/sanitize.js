f9753d2cb6dfa9416d0c78a445ce365fcc0004f0
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-14 15:31:57 -0500

    Added an event listener to the check-in button on the rooms view.

--- public/assets/js/sanitize.js
+++ public/assets/js/sanitize.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
     console.log( "LET'S GET READY TO RUMBLEEEEEEEEEEEEEEEEEEEE!" );
+    
+    $(“#check-in-button”).on(“click”, function (){
+        Const check-in = $(“#check-in-button”)
+        Ch
+        });
+        });
+        Sanitize Code
+        
+        $( document ).ready(function() {
+        console.log( "ready!" );
+        $(“#check-in-button”).on(“click”, function (){
+        Const check-in = $(“#check-in-button”)
+        Ch
+        });
     });
