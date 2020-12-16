a0cf644d444a9e612e115e49f5652f257aea1482
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-16 13:30:57 -0500

    Got the sweet alerts to work for the check-in and check-out buttons. 2 of 3, just need to get the sanitize feature to work.

--- public/assets/js/sanitize.js
+++ public/assets/js/sanitize.js
@@ -21,12 +21,8 @@ $( document ).ready(function() {
                         window.location.replace("/rooms");
                     });
                 });
-        }
-
-// To check out
-    $(".check-in-btn").on("click", function(){
-        if($(this).text() === "Check-Out"){
-            console.log($(this).data("id"));
+        }else if($(this).text() === "Check-Out"){
+            console.log("checking-out now");
             Swal.fire({
                 title: 'Leaving so soon?',
                 text: "Are you sure you want to leave?",
@@ -37,141 +33,30 @@ $( document ).ready(function() {
                 cancelButtonColor: '#d33',
                 confirmButtonText: 'Yes, exit room'
                 }).then((result) =>{
-                if(result.dismiss === Swal.DismissReason.cancel){
-                    $(this).text("Check-Out");
-                    }else if(result.isConfirmed){
+                    if(result.isConfirmed){
                         $(this).text("Sanitize");
-                    Swal.fire({
-                        title: 'Good work! Click Sanitize to clean up the room for the next group.',
-                        icon: 'success',
-                        timer: 5000
-                        });
+                        Swal.fire({
+                            title: 'Good work! Click Sanitize to clean up the room for the next group.',
+                            icon: 'success',
+                            timer: 5000
+                            })
+                    }else{
+                        $(this).text("Check-Out");
+                        Swal.fire({
+                            title: 'No worries,',
+                            text: "Back to studying!",
+                            icon: 'success',
+                            timer: 3000
+                        })
+
                     }
-                        $.ajax({
-                            method: "POST",
-                            url: "/api/rooms",
-                        }).then((response) =>{
-                            window.location.replace("/rooms");
                 })
+        }else if($(this).text() === "Sanitize"){
 
-                        
-                    }
-                 )
-                };
-
+        }
     });
-}
-    );
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $( document ).ready(function() {
-//     console.log( "LET'S GET READY TO RUMBLEEEEEEEEEEEEEEEEEEEE!" );
-//     //To check in
-//     $(".check-in-btn").on("click", function(){
-//         if($(this).text() === "Check-In"){
-//             $(this).text("Check-Out");    
-//             Swal.fire({
-//                     title: 'You have successfully checked into a room',
-//                     text: "Happy studying!",
-//                     icon: 'success',
-//                     timer: 3000
-//                 }).then((result) => {
-//                     $.ajax({
-//                         method: "POST",
-//                         url: "/api/rooms",
-//                     }).then((response) =>{
-//                         window.location.replace("/rooms");
-//                     });
-//                 });
-//         }
-
-// //To check out
-//     $(".check-in-btn").on("click", function(){
-//         if($(this).text() === "Check-Out"){
-//             let id = $(this).data("id");
-//             console.log("id");
-//             Swal.fire({
-//                 title: 'Leaving so soon?',
-//                 text: "Are you sure you want to leave?",
-//                 icon: 'question',
-//                 showCancelButton: true,
-//                 cancelButtonText: 'No, I want to stay',
-//                 confirmButtonColor: '#4962B',
-//                 cancelButtonColor: '#d33',
-//                 confirmButtonText: 'Yes, exit room'
-//                 }).then((result) =>{
-//                 if(result.dismiss === Swal.DismissReason.cancel){
-//                     $(this).text("Check-Out");
-//                     $.ajax({
-//                         method: "POST",
-//                         url: "/api/rooms",
-//                     })
-//                     // .then((response) =>{
-//                     //     window.location.replace("/rooms");
-//                     // });
-//                     }else if(result.isConfirmed){
-//                         $(this).text("Sanitize");
-//                     Swal.fire({
-//                         title: 'Good work! Click Sanitize to clean up the room for the next group.',
-//                         icon: 'success',
-//                         timer: 5000
-//                         });
-//                         $.ajax({
-//                             method: "POST",
-//                             url: "/api/rooms",
-//                         }).then((response) =>{
-//                             window.location.replace("/rooms");
-//                 })
-
-//                         }
-//                     }
-//                  )
-//                 };
-
-//     });
-// }
-//     );
-    
-// });
+    });
+          
 
 
 
