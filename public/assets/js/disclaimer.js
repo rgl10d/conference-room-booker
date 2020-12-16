1a0cd2025b2774ca081a1498a8eacf53b92286c0
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-12-16 02:20:02 -0500

    Moved the COVID disclaimer to it's own js file. Linked the disclaimer.js file to the index so it shows as soon as the website loads.

---
+++ public/assets/js/disclaimer.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+Swal.fire({
+  title: "COVID Disaclaimer:",
+  text:
+    " I acknowledge and agree that by committing to attend the University as an on-campus residential student, I am voluntarily assuming any and all risks that notwithstanding the college's best efforts to implement and require compliance with these prevention and mitigation measures I may be exposed to the coronavirus and may become ill with COVID-19, and that such exposure and illness may result in personal injury, illness, temporary or permanent disability , or even death. By clicking OK, you are legally agreeing to these statements.",
+});
+
+});
--- public/assets/js/newRoom.js
+++ public/assets/js/newRoom.js
@@ -15,9 +15,5 @@ $( document ).ready(function() {
             window.location.replace("/rooms");
         });
     });
-    // Modal function/
-    // Updated the font disclaimer//
-//   Removed the event listener so that the modal pops up whn th page loads
-Swal.fire("Covid Disaclaimer", " I acknowledge and agree that by committing to attend the University as an on-campus residential student, I am voluntarily assuming any and all risks that notwithstanding the college's best efforts to implement and require compliance with these prevention and mitigation measures I may be exposed to the coronavirus and may become ill with COVID-19, and that such exposure and illness may result in personal injury, illness, temporary or permanent disability , or even death. By clicking OK, you are legally agreeing to these statements.");
-});
 
+});
